d4558a384bef1906939e2351d9ffdb7da6a629ec
Andrews <limaandrews@msn.com>
2017-08-03 00:40:27 -0300

    cadastrando um novo titulo

--- src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -2,10 +2,18 @@ package com.algaworks.cobranca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author ksa
+ *
+ */
 @SpringBootApplication
 public class CobrancaApplication {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CobrancaApplication.class, args);
 	}
--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,13 +1,43 @@
 package com.algaworks.cobranca.controller;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.algaworks.cobranca.model.Titulo;
+
+/**
+ * 
+ * @author ksa
+ *
+ */
 @Controller
+@RequestMapping("/titulo")
 public class TituloController {
 
-	@RequestMapping("/titulo/novo")
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		System.out.println(">>>>>>>>>>>>>" + titulo.getDescricao()+ "" + titulo.getCodigo());
+		
+		// TODO: Salvar no banco de dados
 		return "CadastroTitulo";
 	}
 
